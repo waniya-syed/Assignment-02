319c2b180ad26acbbb412d86b73af9734d344629
waniya-syed <waniyasyed2010@gmail.com>
2026-06-21 20:54:34 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// Question # 01
+
+// var age=(16);
+// alert("I am"+age+"years old");
+
+// Question # 02
+// let visits = localStorage.getItem("visitCount");
+// if (visits === null) {
+//     visits = 0;
+// }
+// visits++;
+// localStorage.setItem("visitCount", visits);
+// alert("You have visited this site " + visits + " times");
+
